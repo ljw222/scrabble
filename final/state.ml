2838d11e409b7fefd8f677efc581ec0385c8587f
Lucy <ljw222@cornell.edu>
2020-04-22 01:01:45 -0400

    initial types + added a2 starter code

---
+++ final/state.ml
@@ -0,0 +1,12 @@
+(* Note: You may introduce new code anywhere in this file. *) 
+open Board
+(* TODO: replace [unit] with a type of your own design. *)
+type game_state = 
+  | Ongoing
+  | Done
+
+type t = {
+  winning_score: int;
+  state: game_state;
+  player_turn: Board.players;
+}
---
+++ final/state.mli
@@ -0,0 +1,19 @@
+(** 
+   Representation of dynamic adventure state.
+
+   This module represents the state of an adventure as it is being played,
+   including the adventurer's current room, the rooms that have been visited,
+   and functions that cause the state to change.
+*)
+
+(* You are free to add more code here. *)
+
+(**********************************************************************
+ * DO NOT CHANGE THIS CODE
+ * It is part of the interface the course staff will use to test your 
+ * submission.
+*)
+
+(** The abstract type of values representing the game state. *)
+type t 
+
